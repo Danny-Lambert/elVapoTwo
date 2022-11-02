0b6c597609f447e47cf3afae3306936ab38c6e71
Danny Lambert <dlambert150293@gmail.com>
2022-11-02 17:32:54 +0000

    burger and cart finshed

--- main.js
+++ main.js
@@ -18,9 +18,7 @@ const newVapes = vapeData.map((vape) => {
                 <span class="product-title row">
                 <b>${vape.flavour}</b> <br> 
                 <b>${vape.brand}</b>
-                        <span class="badge col-2">
-                            New
-                        </span>
+
                 </span>
                 </div>
                 <div class="product-properties">
@@ -32,8 +30,10 @@ const newVapes = vapeData.map((vape) => {
                 </div>
                 <span class="product-size mt-2 justify-content-evenly mx-2 row">
                 <button role="button" id="heartButton"   class="button-17 heartButton col-5"><i id="heartButtonIcon" class="fa-solid heartDefaultColour heartButtonIcon fa-heart"></i></button>
-                <button role="button" class="col-5 button-17" style="background:#81ff81;font-weight: bold;">Add to Box</button>
-                </span>
+                <button role="button" class="col-5 button-17 addToBoxButton" style="background:#81ff81;font-weight: bold;">
+                <span class="addTobBoxSpan ">Add to Box</span>
+                <span class="tickButtonIcon d-none " id="tickButtonIcon" style="color: #3a5c4b;">Added <i class="fa-solid  tickButtonIcon fa-check  text-white"></i></span>
+            </button>                </span>
             </div>
             </div>
         </div>
@@ -43,7 +43,6 @@ const newVapes = vapeData.map((vape) => {
         });
 const newVapeBoxes = vapeBoxData.map((vapeBoxes) => {
     return `
-
     <div class="col-9 col-sm-5 col-md-4 col-lg-3 mx-2" >
         <div class="card-new-card  mb-5 px-0">
         <div class="card-head-new-card d-flex align-items-center h-100">
@@ -55,7 +54,6 @@ const newVapeBoxes = vapeBoxData.map((vapeBoxes) => {
             <div class="product-desc">
             <span class="product-title row">
             <b>${vapeBoxes.flavour}</b> <br> 
-            <b>${vapeBoxes.brand}</b>
                     <span class="badge col-2">
                         New
                     </span>
@@ -63,7 +61,6 @@ const newVapeBoxes = vapeBoxData.map((vapeBoxes) => {
             </div>
             <div class="product-properties">
             <span class="product-size mb-3 ms-2 row ms-0 col-12">
-                <p class="col-auto ">Nicotine -</p>
                 <p class="col-auto ps-0 mx-2">${vapeBoxes.description}</p>
             </span>
             <br/>
@@ -71,8 +68,8 @@ const newVapeBoxes = vapeBoxData.map((vapeBoxes) => {
             <span class="product-size mt-2 justify-content-evenly mx-2 row">
             <button role="button" id="heartButton"   class="button-17 heartButton col-5"><i id="heartButtonIcon" class="fa-solid heartDefaultColour heartButtonIcon fa-heart"></i></button>
             <button role="button" class="col-5 button-17 addToBoxButton" style="background:#81ff81;font-weight: bold;">
-                <span class="addTobBoxSpan">Add to Box</span>
-                <i id="tickButtonIcon" class="fa-solid  tickButtonIcon fa-check d-none text-white">Added</i>
+                <span class="addTobBoxSpan">Add to Basket</span>
+                <span class="tickButtonIcon d-none " id="tickButtonIcon" style="color: #3a5c4b;">Added <i class="fa-solid  tickButtonIcon fa-check  text-white"></i></span>
             </button>
             </span>
         </div>
@@ -97,6 +94,10 @@ const newVapeBoxes = vapeBoxData.map((vapeBoxes) => {
     $(".heartButton").click(function(){
         $(this).children().toggleClass("text-danger")
         $(this).children().toggleClass("heartDefaultColour")
+      });
 
+      $(".details").click(function(){
+        // $(this).children().toggleClass("text-danger")
+        alert("click")
       });
 
